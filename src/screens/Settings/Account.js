6d5ae3bf996a2a857f93fe8eb088e18ffb0388d1
RossBunn <rbunn1997@gmail.com>
2022-01-20 16:34:13 +0000

    styled the account page more

--- src/screens/Settings/Account.js
+++ src/screens/Settings/Account.js
@@ -1,70 +1,132 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text} from 'react-native'
 import {Navigation} from '@react-navigation/native';
 import { TextButton } from '../../components';
 import {icons, FONTS, SIZES, COLORS} from '../../constants';
-import { white } from 'react-native-paper/lib/typescript/styles/colors';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Account = () => {
     return (
         
-        <View>
+        <View style={{
+
+        }}>
             {/* if user is loged in */}
-            <Text>My Account</Text>
-            <Text>
-                Welcome {/*{User.name}*/}
-            </Text>
-            <TextButton
-                label="Edit Profile"
-                labelStyle={{
+            <View style={{
+            }}>
+                <Text style={{
+                    padding: 22,
+                    backgroundColor:Colors.white,
+                    fontSize: SIZES.body2,
+                    alignItem: 'center',
                     color: COLORS.black,
-                    justifyContent:'flex-start',
-                }}
-                buttonContainerStyle={{
-                    backgroundColor: COLORS.white,
-                    justifyContent:'flex-start',
-                  }}
-            />
-            <TextButton
-                label="Saved Addresses"
-                labelStyle={{
+                    justifyContent: 'flex-start'
+                }}>My Account</Text>
+            </View>
+            <View>
+                <Text style={{
+                    padding: 22,
+                    backgroundColor:Colors.white,
+                    fontSize: SIZES.body2,
+                    alignItem: 'center',
                     color: COLORS.black,
-                }}
-                buttonContainerStyle={{
-                    backgroundColor: COLORS.white,
-                  }}
-            />
+                    justifyContent: 'flex-start',
+                    bordertop: 5,
+                }}>
+                    Welcome {/*{User.name}*/}
+                </Text>
+            </View>
+            <View>
+                <TextButton
+                    iconName="person-outline"
+                    label="Edit Profile"
+                    iconSize={24}
+                    labelStyle={{
+                        color: COLORS.black,
+                        justifyContent:'flex-start',
+                    }}
+                    buttonContainerStyle={{
+                        backgroundColor: COLORS.white,
+                        height: 50,
+                        alignItems: 'flex-start',
+                        justifyContent: 'center',
+                        // flexDirection:'row-reverse'
+                    }}
+                />
+                <TextButton
+                    iconName="push-pin"
+                    label="Saved Addresses"
+                    iconSize={24}
+                    labelStyle={{
+                        color: COLORS.black,
+                    }}
+                    buttonContainerStyle={{
+                        backgroundColor: COLORS.white,
+                        height: 50,
+                        alignItems: 'flex-start',
+                        justifyContent: 'center',
+                    }}
+                />
+                <TextButton
+                    iconName="bell"
+                    label="Contact Preferences"
+                    iconSize={24}
+                    labelStyle={{
+                        color: COLORS.black,
+                    }}
+                    buttonContainerStyle={{                        
+                        backgroundColor: COLORS.white,
+                        height: 50,
+                        alignItems: 'flex-start',
+                        justifyContent: 'center',
+                    }}
+                />
+                <TextButton
+                    iconName="login"
+                    label="Sign Out"
+                    iconSize={24}
+                    labelStyle={{
+                        color: COLORS.black,
+                    }}
+                    buttonContainerStyle={{
+                        backgroundColor: COLORS.white,
+                        height: 50,
+                        alignItems: 'flex-start',
+                        justifyContent: 'center',
+                    }}
+                />
+            </View>
+             <View
+              style={{
+                flexDirection: 'row',
+                marginTop: SIZES.radius,
+                justifyContent: 'center',
+              }}>
             <TextButton
-                label="Contact Preferences"
-                labelStyle={{
-                    color: COLORS.black,
+                iconName="headset-mic"
+                iconSize={24}
+                iconStyle={{
+                    paddingRight: SIZES.base,
+                    justifyContent: 'flex-start',
+                    alignItems:'flex-start',
                 }}
-                buttonContainerStyle={{
-                    backgroundColor: COLORS.white,
-                  }}
-            />
-            <TextButton
-                label="Sign Out"
-                labelStyle={{
-                    color: COLORS.black,
-                }}
-                buttonContainerStyle={{
-                    backgroundColor: COLORS.white,
-                  }}
-            />
-            <TextButton
                 label="Click here for help"
+                labelStyle={{
+                    justifyContent: 'flex-end'
+                }}
                 buttonContainerStyle={{
                     height: 50,
                     width: SIZES.width / 2,
-                    alignItems: 'center',
+                    alignItems: 'flex-end',
+                    justifyContent: 'center',
                     marginTop: SIZES.padding * 2,
                     borderRadius: SIZES.radius + 5,
                     backgroundColor: COLORS.primary,
                   }}
                 onPress={() => navigation.navigate('')}
             />
+            </View>
             {/* if user is not loged in */}
         </View>
     )
