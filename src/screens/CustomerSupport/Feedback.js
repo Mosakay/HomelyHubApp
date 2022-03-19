9e7afd30ba74b83ec96091ef3b34cd7cdcb25c40
RossBunn <rbunn1997@gmail.com>
2022-03-19 17:00:28 +0000

    made the basic layout need to style and add in two more parts

--- src/screens/CustomerSupport/Feedback.js
+++ src/screens/CustomerSupport/Feedback.js
@@ -3,11 +3,16 @@ import React, {useState} from 'react';
 import {COLORS, FONTS, SIZES} from '../../constants/theme';
 import {ScrollView, TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {TextButton} from '../../components';
 
 const Feedback = ({navigation, props}) => {
 
   return (
-    <View>
+    <View
+      style={{
+        flex: 1
+      }}
+    >
         <ScrollView>
             <View
                 style={{
@@ -29,6 +34,73 @@ const Feedback = ({navigation, props}) => {
                 style={{...FONTS.h3, color: COLORS.black, marginTop: SIZES.padding}}>
                 Feedback
                 </Text>
+            </View>
+            {/* review expereance */}
+            <View
+              style={{
+                marginHorizontal: SIZES.padding,
+                marginTop:SIZES.base,
+                backgroundColor:COLORS.white,
+              }}
+            >
+              <View>
+                <Text>
+                  Rate your Experience
+                </Text>
+                <Text>Are you satisfied with our customer service?</Text>
+                {/* Starts witch light and delight where you click */}
+                <View>
+
+                </View>                
+              </View>
+              <View>
+                <Text>
+
+                </Text>
+                  <TouchableOpacity>
+                    {/* buttons */}
+                  </TouchableOpacity>
+              </View>
+            </View>
+            {/* tell us what can be improved */}
+
+            <View
+              style={{
+                marginHorizontal: SIZES.padding,
+                marginTop:SIZES.base,
+                backgroundColor:COLORS.white,
+              }}             
+            >  
+            <Text>
+                Tell us what can be improved
+              </Text>
+              <View style={{
+                  minHeight:200,
+
+              }}>
+                  <TextInput 
+                      multiline 
+                      placeholder="Tell us on how we can improve..."
+                      style={{
+                          maxHeight: 100, 
+                          padding: 10, 
+                          width: "100%",
+                      }} 
+                  />
+              </View>
+            </View>
+            {/* continue button */}
+            <View>
+            <TextButton
+              label="Continue"
+              buttonContainerStyle={{
+                padding: 15,
+                width: SIZES.width - 200,
+                borderRadius:SIZES.radius,
+              }}
+              labelStyle={{marginLeft: 5, ...FONTS.body4, color: COLORS.darkGray}}
+              // onPress={() => navigation.navigate('EditProfile')}
+            />
             </View>
         </ScrollView>
     </View>
