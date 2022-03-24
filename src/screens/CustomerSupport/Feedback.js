440fabc0dde768258e75355d3b64daa492657883
RossBunn <rbunn1997@gmail.com>
2022-03-24 14:46:49 +0000

    added in the review and the buttons

--- src/screens/CustomerSupport/Feedback.js
+++ src/screens/CustomerSupport/Feedback.js
@@ -4,13 +4,20 @@ import {COLORS, FONTS, SIZES} from '../../constants/theme';
 import {ScrollView, TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {TextButton} from '../../components';
+import { Rating, AirbnbRating } from 'react-native-ratings';
+
 
 const Feedback = ({navigation, props}) => {
+
+  const WATER_IMAGE = require('../../assets/icons/star.png');
+
 
   return (
     <View
       style={{
-        flex: 1
+        display:'flex',
+        flex:1,
+        backgroundColor:COLORS.white,
       }}
     >
         <ScrollView>
@@ -38,19 +45,47 @@ const Feedback = ({navigation, props}) => {
             {/* review expereance */}
             <View
               style={{
+                height:10,
+                backgroundColor:COLORS.lightGray1
+              }}
+            >
+              {/* spacer */}
+            </View>
+            <View
+              style={{
                 marginHorizontal: SIZES.padding,
                 marginTop:SIZES.base,
-                backgroundColor:COLORS.white,
+                // backgroundColor:COLORS.white,
               }}
             >
               <View>
-                <Text>
+                <Text
+                  style={{
+                    fontSize:SIZES.h2,
+                    marginTop:SIZES.padding *2,
+                    color:COLORS.black, 
+                  }}
+                >
                   Rate your Experience
                 </Text>
-                <Text>Are you satisfied with our customer service?</Text>
+                <Text
+                  style={{
+                    fontSize:SIZES.h5,
+                    color:COLORS.black,
+                    paddingBottom:SIZES.padding
+                  }}
+                >Are you satisfied with our service?</Text>
                 {/* Starts witch light and delight where you click */}
-                <View>
 
+                <View>
+                  <AirbnbRating
+                    count={5}
+                    defaultRating={2}
+                    size={40}
+                    showRating={false}
+                    selectedColor={COLORS.primary}
+
+                  />
                 </View>                
               </View>
               <View>
@@ -63,6 +98,14 @@ const Feedback = ({navigation, props}) => {
               </View>
             </View>
             {/* tell us what can be improved */}
+            <View
+              style={{
+                height:15,
+                backgroundColor:COLORS.lightGray1
+              }}
+            >
+              {/* spacer */}
+            </View>
 
             <View
               style={{
@@ -71,11 +114,85 @@ const Feedback = ({navigation, props}) => {
                 backgroundColor:COLORS.white,
               }}             
             >  
-            <Text>
+            <Text
+            style={{
+              fontSize:SIZES.h2,
+              color:COLORS.black,
+            }}
+            >
                 Tell us what can be improved
               </Text>
+              <View>
+                <View
+                  style={{
+                    justifyContent:'space-between'
+                  }}
+                >
+                  <TextButton
+                    label=" Overall service"
+                    buttonContainerStyle={{
+                      paddingVertical: 5, 
+                      backgroundColor:COLORS.lightGray1,
+                      maxWidth:'30%',
+                      borderRadius:SIZES.radius,
+                    }}
+                    labelStyle={{
+                      ...FONTS.body5,
+                      color: COLORS.darkGray,
+                    }}
+                    // onPress={() => navigation.navigate('ContactPref')}
+                  />
+                   <TextButton
+                  label="Pickup and delivery Service"
+                  buttonContainerStyle={{
+                    paddingVertical: 5, 
+                    backgroundColor:COLORS.lightGray1,
+                    maxWidth:'55%',
+                    borderRadius:SIZES.radius,
+                  }}
+                  labelStyle={{
+                    ...FONTS.body5,
+                    color: COLORS.darkGray,
+                  }}
+                  // onPress={() => navigation.navigate('ContactPref')}
+                />                  
+                </View>
+                <View>
+                <TextButton
+                  label="Speed and Efficency"
+                  buttonContainerStyle={{
+                    paddingVertical: 5, 
+                    backgroundColor:COLORS.lightGray1,
+                    maxWidth:'50%',
+                    borderRadius:SIZES.radius,
+                  }}
+                  labelStyle={{
+                    ...FONTS.body4,
+                    color: COLORS.darkGray,
+                  }}
+                  // onPress={() => navigation.navigate('ContactPref')}
+                />
+                  <TextButton
+                    label="Customer Support"
+                    buttonContainerStyle={{
+                      paddingVertical: 5, 
+                      backgroundColor:COLORS.lightGray1,
+                      maxWidth:'45%',
+                      borderRadius:SIZES.radius,
+                    }}
+                    labelStyle={{
+                      ...FONTS.body4,
+                      color: COLORS.darkGray,
+                    }}
+                    // onPress={() => navigation.navigate('ContactPref')}
+                  />               
+                </View>
+
+              </View>
               <View style={{
                   minHeight:200,
+                  backgroundColor:COLORS.lightGray1,
+                  borderRadius:SIZES.radius,
 
               }}>
                   <TextInput 
@@ -90,18 +207,37 @@ const Feedback = ({navigation, props}) => {
               </View>
             </View>
             {/* continue button */}
-            <View>
-            <TextButton
-              label="Continue"
-              buttonContainerStyle={{
-                padding: 15,
-                width: SIZES.width - 200,
-                borderRadius:SIZES.radius,
-              }}
-              labelStyle={{marginLeft: 5, ...FONTS.body4, color: COLORS.darkGray}}
-              // onPress={() => navigation.navigate('EditProfile')}
-            />
+            <View
+            >
+              {/* spacer */}
             </View>
+            <View
+              style={{
+                marginTop:SIZES.padding,
+                backgroundColor:COLORS.lightGray1
+              }}
+            >
+              <View
+                style={{
+                  justifyContent:'center',
+                  alignSelf:'center',
+                  alignItems:'center',
+                  marginTop:SIZES.padding
+                }}
+              >
+              <TextButton
+                label="Continue"
+                buttonContainerStyle={{
+                  padding: 15,
+                  width: SIZES.width - 200,
+                  borderRadius:SIZES.radius,
+                }}
+                labelStyle={{marginLeft: 5, ...FONTS.body4, color: COLORS.darkGray}}
+                // onPress={() => navigation.navigate('EditProfile')}
+              />
+              </View>
+            </View>
+
         </ScrollView>
     </View>
   );
